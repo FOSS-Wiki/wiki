1eeff986e31bc4d752c0d2a68d2849b554bb09b5
Atmois <info@atmois.com>
2026-05-11 18:18:56 +0100

    fix(groups): refine permissions

--- wiki/configs/70-UserRights.php
+++ wiki/configs/70-UserRights.php
@@ -19,7 +19,7 @@
 $wgGroupPermissions['*']['createaccount'] = true;
 $wgGroupPermissions['*']['edit'] = false;
 
-// user ~ All registered users
+// user ~ Registered Users
 $wgGroupPermissions['user']['edit'] = true;
 $wgGroupPermissions['user']['writeapi'] = true; // For visual editor
 $wgGroupPermissions['user']['move'] = false;
@@ -28,17 +28,17 @@ $wgGroupPermissions['user']['move-categorypages'] = false;
 $wgGroupPermissions['user']['move-rootuserpages'] = false;
 $wgGroupPermissions['user']['movefile'] = false;
 
-// autoconfirmed ~ Given for wiki author role sync on discord
+// autoconfirmed ~ Wiki Editor Role
 $wgAutoConfirmAge = 259200;
 $wgAutoConfirmCount = 10;
 $wgGroupPermissions['autoconfirmed']['autoconfirmed'] = false;
 $wgGroupPermissions['autoconfirmed']['editsemiprotected'] = false;
 
-// template-editor ~ Grants edit permissions for templates and modules
+// template-editor ~ Template and Module Editors
 $wgGroupPermissions['template-editor']['template-editing'] = true;
 $wgGroupPermissions['template-editor']['module-editing'] = true;
 
-// wiki-team ~ For Wiki Team Members
+// wiki-team ~ FOSS Wiki Staff
 $wgGroupPermissions['wiki-team']['template-editing'] = true;
 $wgGroupPermissions['wiki-team']['module-editing'] = true;
 $wgGroupPermissions['wiki-team']['meta-editing'] = true;
@@ -54,7 +54,7 @@ $wgGroupPermissions['wiki-team']['rollback'] = true;
 $wgAddGroups['wiki-team'] = array('template-editor', 'autoconfirmed');
 $wgRemoveGroups['wiki-team'] = array('template-editor', 'autoconfirmed', "wiki-team");
 
-// sysop ~ Only for Wiki Admins, gives ALL permissions
+// sysop ~ FOSS Wiki Administrators
 $wgGroupPermissions['sysop']['checkuser'] = true;
 $wgGroupPermissions['sysop']['checkuser-log'] = true;
 $wgGroupPermissions['sysop']['investigate'] = true;
@@ -71,31 +71,34 @@ $wgGroupPermissions['sysop']['editinterface'] = true;
 $wgGroupPermissions['sysop']['template-editing'] = true;
 $wgGroupPermissions['sysop']['module-editing'] = true;
 $wgGroupPermissions['sysop']['meta-editing'] = true;
-$wgGroupPermissions['sysop']['hideuser'] = true;
-$wgGroupPermissions['sysop']['deletelogentry'] = true;
-$wgGroupPermissions['sysop']['deleterevision'] = true;
-$wgGroupPermissions['sysop']['suppressionlog'] = true;
-$wgGroupPermissions['sysop']['viewsuppressed'] = true;
-$wgGroupPermissions['sysop']['suppressrevision'] = true;
-$wgGroupPermissions['sysop']['usermerge'] = true;
-$wgGroupPermissions['sysop']['mwoauthproposeconsumer'] = true;
-$wgGroupPermissions['sysop']['mwoauthupdateownconsumer'] = true;
-$wgGroupPermissions['sysop']['mwoauthmanageconsumer'] = true;
-$wgGroupPermissions['sysop']['mwoauthsuppress'] = true;
-$wgGroupPermissions['sysop']['mwoauthviewsuppressed'] = true;
-$wgGroupPermissions['sysop']['mwoauthviewprivate'] = true;
-$wgGroupPermissions['sysop']['mwoauthmanagemygrants'] = true;
 $wgGroupPermissions['sysop']['interwiki'] = true;
+$wgGroupPermissions['sysop']['block'] = true;
+$wgGroupPermissions['sysop']['rollback'] = true;
 $wgGroupPermissions['sysop']['import'] = false;
 
 $wgAddGroups['sysop'] = array('moderator', 'autoconfirmed','template-editor', 'wiki-team');
 $wgRemoveGroups['sysop'] = array('moderator', 'autoconfirmed', 'template-editor', 'wiki-team', 'sysop');
 
-// bureaucrat ~ FOSS Wiki Management
+// bureaucrat ~ FOSS Wiki Directors
 $wgGroupPermissions['bureaucrat']['userrights'] = true;
 $wgGroupPermissions['bureaucrat']['userrights-interwiki'] = true;
 $wgGroupPermissions['bureaucrat']['renameuser'] = true;
 $wgGroupPermissions['bureaucrat']['block'] = true;
+$wgGroupPermissions['bureaucrat']['suppressrevision'] = true;
+$wgGroupPermissions['bureaucrat']['hideuser'] = true;
+$wgGroupPermissions['bureaucrat']['deletelogentry'] = true;
+$wgGroupPermissions['bureaucrat']['deleterevision'] = true;
+$wgGroupPermissions['bureaucrat']['suppressionlog'] = true;
+$wgGroupPermissions['bureaucrat']['viewsuppressed'] = true;
+$wgGroupPermissions['bureaucrat']['usermerge'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthproposeconsumer'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthupdateownconsumer'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthmanageconsumer'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthsuppress'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthviewsuppressed'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthviewprivate'] = true;
+$wgGroupPermissions['bureaucrat']['mwoauthmanagemygrants'] = true;
+$wgGroupPermissions['bureaucrat']['import'] = false;
 
 $wgAddGroups['bureaucrat'] = array('autoconfirmed', 'template-editor', 'wiki-team', 'sysop', 'bureaucrat');
 $wgRemoveGroups['bureaucrat'] = array('autoconfirmed', 'template-editor', 'wiki-team', 'sysop', 'bureaucrat');
